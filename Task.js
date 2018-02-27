13175919b799fd9a56c91749779d3f30db80a99b
Your Name <you@example.com>
2018-02-28 00:58:35 +0200

    parse file to lines

--- Task.js
+++ Task.js
@@ -7,4 +7,13 @@ function main(){
 
     //1.read csv file
     let data = fs.readFileSync('./data.csv','utf-8');
+    //2.parse csv file to lines 
+    let lines = parseCsvFileToLines(data);
+
+}
+
+function parseCsvFileToLines(data){
+    let lines = data.split("\n");
+    //console.log(lines);
+    return lines;
 }
