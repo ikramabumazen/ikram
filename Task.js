f14543be1dd02b1d8d6f795970941827c6a97895
Your Name <you@example.com>
2018-02-28 01:05:22 +0200

    splitLinesToHeadersAndKeys

--- Task.js
+++ Task.js
@@ -9,6 +9,10 @@ function main(){
     let data = fs.readFileSync('./data.csv','utf-8');
     //2.parse csv file to lines 
     let lines = parseCsvFileToLines(data);
+    //3.split lines to Headers and keys
+    let { headers , keys}= splitLinesToHeadersAndKeys(lines);
+
+    
 
 }
 
@@ -16,4 +20,10 @@ function parseCsvFileToLines(data){
     let lines = data.split("\n");
     //console.log(lines);
     return lines;
+}
+
+function splitLinesToHeadersAndKeys(lines){
+    let headers = lines.shift();
+    let keys = headers.split(",");
+    return { headers, keys }
 }
